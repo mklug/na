81dc36b32c25a6d30f6e8439939956fb27f35910
Michael Klug <mcihael.r.klug@gmail.com>
2025-01-23 16:06:43 -0600

    Cleaned up the indexing

--- tea/3/divided_diff.m
+++ tea/3/divided_diff.m
@@ -1,12 +1,12 @@
 function F = divided_diff(x, y)
 
-    N = length(x) - 1;
-    for i=1:(N+1)
+    N = length(x);
+    for i=1:N
         F(i,1) = y(i);
     end%for
 
-    for j=2:(N+1)
-        for i=1:(N+2-j)
+    for j=2:N
+        for i=1:(N+1-j)
             F(i,j) = (F(i,j-1)-F(i+1,j-1)) / ...
                      (x(i) - x(i+j-1));
         end%for
