3eca6bde070f2c0e199784426c7e98499a7bb696
Michael Klug <michael.r.klug@gmail.com>
2025-02-03 12:55:37 -0600

    added integration techniques

--- tea/4/h_small_experiment.m
+++ tea/4/h_small_experiment.m
@@ -2,11 +2,10 @@ format('long')
 f = @(x) sin(x);
 f_der = @(x) cos(x);
 f_der_approx = @(x,h) (f(x+h) - f(x-h)) / (2*h);
+
 xhat = 1.0;
 
-disp("Actual value:")
-actual = f_der(xhat);
-actual
+actual = f_der(xhat)
 
 hs = [1, 0.1, 0.01, 0.001, 0.0001, ...
       0.00001, 0.000001, 0.0000001, ...
@@ -14,6 +13,6 @@ hs = [1, 0.1, 0.01, 0.001, 0.0001, ...
 
 for h = hs
     h
-    approx = f_der_approx(xhat, h)
+    approx = f_der_approx(xhat, h);
     abs_error = abs(actual - approx)
 end%for
