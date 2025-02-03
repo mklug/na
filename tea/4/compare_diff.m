756e1d8ad77e00338cb84b3bbf9b10c269f4edc4
Michael Klug <michael.r.klug@gmail.com>
2025-02-03 12:55:37 -0600

    added 3 point forward to comparison

--- tea/4/compare_diff.m
+++ tea/4/compare_diff.m
@@ -4,20 +4,24 @@ df_exact = @(x) cos(x);
 for_diff = @(x, h) (f(x + h) - f(x)) / h;
 cen_diff = @(x, h) (f(x+h) - f(x - h)) / (2*h);
 bac_diff = @(x, h) (f(x) - f(x-h)) / h; 
+for_diff_3 = @(x,h) (-3*f(x)+4*f(x+h)-f(x+2*h))/(2*h);
 
 x = 0:0.1:2*pi;
 h = 0.1;
 dfdx_f = for_diff(x, h); % "Implicit expansion"
 dfdx_c = cen_diff(x, h);
 dfdx_b = bac_diff(x, h);
+dfdx_f3 = for_diff_3(x, h);
 
 plot(x, dfdx_f, 'b');
 hold on;
 plot(x, dfdx_c, 'g');
 plot(x, dfdx_b, 'k');
 plot(x, df_exact(x), 'r');
+plot(x, dfdx_f3, 'c')
 
 legend('forward',  'centered', 
-       'backward', 'exact')
+       'backward', 'exact',
+       '3 point forward')
 
 hold off;
